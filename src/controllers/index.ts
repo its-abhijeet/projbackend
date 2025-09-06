d3a1c86d134abf6cfe8461ddbd9ec0cc78ac24da
Ujjwal Kumar Singh <ujjwalkumar0149@gmail.com>
2025-09-06 16:01:02 +0530

    Added auth controllers

---
+++ src/controllers/index.ts
@@ -0,0 +1,9 @@
+import AuthController from "./AuthController";
+import ProductController from "./ProductController";
+import SellerController from "./SellerController";
+import BuyerController from "./BuyerController";
+import ChatLeadController from "./ChatLeadController";
+
+export {
+AuthController, ProductController, SellerController, BuyerController, ChatLeadController
+}
